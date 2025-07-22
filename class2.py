d6b0ae254c2fe2c3f46bf7da373e739b789bca30
Student <Student@Train11-5-PC282.control.ad.sit.kmutt.ac.th>
2025-07-22 15:25:15 +0700

    Add Class2

---
+++ class2.py
@@ -0,0 +1,58 @@
+# age = 18
+# print(age)
+
+# num = 5
+# print(type(num))
+
+# num2 = 3.14
+# print(type(num2))
+
+# num3 = "3.14"
+# print(type(num3))
+
+# booleen = True
+# print(type(booleen))
+
+# age = "18.5"
+# print(type(age))
+
+# age2 = float(age)
+# print(type(age2))
+
+# age3=int(age)
+# print(type(age3))
+
+# tf = "True"
+# print(type(tf))
+
+# tf1 = bool(tf)
+# print(type(tf1))
+
+# sum = 1+2
+# print(sum)
+
+# sum2 = sum - 1
+# print(sum2)
+
+# sum3 = sum * 2
+# print(sum3)
+
+# sum4 = sum3 / sum
+# print(sum4)
+
+# sum = False == True
+# print(sum)
+
+# Name = input("ใส่nameของคุณ : ")
+# print("ur name is ", Name)
+
+# x = int(input("Put the number : "))
+# x = x % 2
+
+# if x == 0:
+#     print("even num")
+# elif x == 1:
+#     print("odd num")
+# else:
+#     print("WHAT I S Thatman")
+
