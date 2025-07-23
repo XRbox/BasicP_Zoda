c9be27749b879660dc3b55799adfe593bec415e6
Student <Student@Train11-5-PC282.control.ad.sit.kmutt.ac.th>
2025-07-23 09:06:19 +0700

    FIX CONDITION

--- class2_workshop.py
+++ class2_workshop.py
@@ -1,17 +1,32 @@
 #สงครามรันด่วน
 Distance = int(input("ระยะทางของคุณเท่าไร : "))
 
-if Distance < 0: 
-    print("ส่งย้อนกลับหรอไอขี้หมา")
-elif Distance <= 50 and Distance > 4:
-    print("10 บาท")
-elif Distance <= 100 and Distance > 50:
-    print("15 บาท")
-elif Distance <= 300 and Distance > 100:
-    print("25 บาท")
-elif Distance <= 500 and Distance > 300:
-    print("35 บาท")
-elif Distance > 500 :
-    print("45 บาท")
-else:
+# if Distance < 0: 
+#     print("ส่งย้อนกลับหรอไอขี้หมา")
+# elif Distance <= 50 and Distance > 4:
+#     print("10 บาท")
+# elif Distance <= 100 and Distance > 50:
+#     print("15 บาท")
+# elif Distance <= 300 and Distance > 100:
+#     print("25 บาท")
+# elif Distance <= 500 and Distance > 300:
+#     print("35 บาท")
+# elif Distance > 500 :
+#     print("45 บาท")
+# else:
+#     print("ไม่ส่งให้พวกขี้หมา")
+
+if Distance > 500:
+    print("45บาทนะจ๊ะ")
+elif Distance > 300:
+    print("35บาทนะจ๊ะ")
+elif Distance > 100:
+    print("25บาทนะจ๊ะ")
+elif Distance > 50:
+    print("15บาทนะจ๊ะ")
+elif Distance > 4:
+    print("10บาทนะจ๊ะ")
+elif Distance >= 0:
     print("ไม่ส่งให้พวกขี้หมา")
+else:
+    print("มาเกรียนไรแถวสนี้ ไอEASY")
