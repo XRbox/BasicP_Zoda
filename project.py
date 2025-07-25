0992ed75d67f9aa315da75241b85c5a8629c5bdd
XRbox <164211100+XRbox@users.noreply.github.com>
2025-07-25 11:51:06 +0700

    last_version

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-def main():
+def loop(num):
     menu_list = [
         {'name' : 'น้ำปั่นรองเท้าแตะ','price' : 10},
         {'name' : 'น้ำแมลงปอปั่น' , 'price' : 15},
@@ -21,25 +21,30 @@ def main():
         {'name' : 'S','price' : 40},
         {'name' : 'M' , 'price' : 60},
         {'name' : 'L' , 'price' : 85}]
-    
-    show_menu(menu_list)
-    choice = int(input("เลือกเมนูที่ต้องการ : ")) - 1
-    checkInList(choice , menu_list)
-    show_sweet(sweet)
-    choice_sweet = int(input("เลือกความหวานตัดขาของคุณเลย : ")) - 1
-    checkInList(choice_sweet , sweet)
-    show_topping(topping)
-    choice_topping = int(input("เลือกท็อปปิ้งที่คุณต้องการ : ")) - 1
-    checkInList(choice_topping , topping)
-    show_size(size)
-    choice_size = int(input("เลือกขนาดที่คุณต้องการ : ")) - 1
-    checkInList(choice_size , size)
-    quantity = int(input("ลูกค้าจะสั่งกี่แก้วคะ : "))
 
-    results = calculator(menu_list[choice]['price'] , sweet[choice_sweet]['price'] , topping[choice_topping]['price'] , size[choice_size]['price'] , quantity)
-    print(f"รายการที่ต้องชำระคือ : {menu_list[choice]['name']}\n เพิ่ม : {topping[choice_topping]['name']}\n หวาน : {sweet[choice_sweet]['name']}\n ขนาด : {size[choice_size]['name']}")
+    results = 0
+    for i in range(num):
+        show_menu(menu_list)
+        choice = int(input("เลือกเมนูที่ต้องการ : ")) - 1
+        checkInList(choice , menu_list)
+        show_sweet(sweet)
+        choice_sweet = int(input("เลือกความหวานตัดขาของคุณเลย : ")) - 1
+        checkInList(choice_sweet , sweet)
+        show_topping(topping)
+        choice_topping = int(input("เลือกท็อปปิ้งที่คุณต้องการ : ")) - 1
+        checkInList(choice_topping , topping)
+        show_size(size)
+        choice_size = int(input("เลือกขนาดที่คุณต้องการ : ")) - 1
+        checkInList(choice_size , size)
+        results += calculator(menu_list[choice]['price'] , sweet[choice_sweet]['price'] , topping[choice_topping]['price'] , size[choice_size]['price'])
+        print(f"รายการที่ต้องชำระคือ : {menu_list[choice]['name']}\n เพิ่ม : {topping[choice_topping]['name']}\n หวาน : {sweet[choice_sweet]['name']}\n ขนาด : {size[choice_size]['name']}")
+        dot_line()
+    return results
+def main():
+    quan = int(input("สั่งกี่แก้ว : "))
+    result = loop(quan)
     dot_line()
-    print("ราคารวมคือ" , results)
+    print("ราคารวมคือ" , result)
 
 def show_menu(menus):
     for i ,menu in enumerate(menus):
@@ -61,12 +66,12 @@ def show_size(sizes):
         print(f"{i +  1}" , size['name'] , size['price'])
     dot_line()
 
-def calculator(price_menu , price_sweet , price_topping , price_size , quan):
+def calculator(price_menu , price_sweet , price_topping , price_size):
     if price_sweet == 'FREE':
         price_sweet = 0
     if price_topping == 'FREE':
         price_topping = 0
-    result = (price_menu + price_sweet + price_topping + price_size) * quan
+    result = (price_menu + price_sweet + price_topping + price_size)
     return result
 
 def dot_line():
